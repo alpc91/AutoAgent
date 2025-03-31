bd8ffd5d39a58aac300985e8f9711a25aab96b37
alpc91 <459388027@qq.com>
2025-03-31 10:40:56 +0800

    v4

--- autoagent/cli_utils/metachain_meta_workflow.py
+++ autoagent/cli_utils/metachain_meta_workflow.py
@@ -11,6 +11,16 @@ from autoagent.agents.meta_agent.workflow_former import get_workflow_former_agen
 from autoagent.agents.meta_agent.workflow_creator import get_workflow_creator_agent
 import re
 from autoagent.agents.meta_agent.worklow_form_complie import parse_workflow_form, WorkflowForm
+from autoagent.types import Result
+from autoagent.memory.rag_memory import Memory
+import math
+import os
+from typing import Union
+from autoagent.environment.docker_env import DockerEnv
+from autoagent.environment.local_env import LocalEnv
+from autoagent.environment.markdown_browser.mdconvert import MarkdownConverter
+import zipfile
+from autoagent.memory.utils import chunking_by_token_size
 
 def workflow_generating(workflow_generator, client, messages, context_variables, requirements, debug):
     messages.append({"role": "user", "content": requirements + """
@@ -132,19 +142,122 @@ The last attempt failed with the following error: {content}, please try again to
         return f"The desired workflow is not created or tested successfully with {MAX_RETRY} attempts.", messages
 
 
+def save_raw_docs_to_vector_db(context_variables: dict, doc_name: str, saved_vector_db_name: str, overwrite: bool = False) -> Result:
+    """
+    Save the raw documents to the vector database. The documents could be: 
+    - ANY text document with the extension of pdf, docx, txt, etcs.
+    - A zip file containing multiple text documents
+    - a directory containing multiple text documents
+    All documents will be converted to raw text format and saved to the vector database in the chunks of 4096 tokens.
+    
+    Args:
+        doc_name: The name of the raw documents. All documents will be stored in the the directory: /workplace/docs. 
+        [NOTES] doc_name should  be the name of the file or directory, not the path to the file or directory, which means `docs/dir_name/` is not a valid doc_name.
+        saved_vector_db_name: the name of collection you want to save the documents to.
+        overwrite: Whether to overwrite the existing vector database when the vector database of the documents already exists. (default: False)
+    """
+    try:
+        memo: Memory = context_variables.get("memo", Memory(project_path=os.path.join(os.getcwd(), "user_db"), db_name = ".user_db"))
+        assert memo is not None, "memo is not set"
+        code_env: Union[DockerEnv, LocalEnv] = context_variables.get("code_env", LocalEnv())
+        assert code_env is not None, "code_env is not set"     
+
+        # check if the saved_vector_db_name is already in the vector database
+        if memo.count(saved_vector_db_name) > 0:
+            if overwrite:
+                prefix_res = f"[WARNING] The collection `{saved_vector_db_name}` of the vector database already exists. Overwriting the existing collection."
+            else: 
+                return f"[WARNING] The collection `{saved_vector_db_name}` of the vector database already exists. Please set the overwrite flag to True if you want to overwrite the existing collection."
+        else:
+            prefix_res = ""
+
+        doc_dir = os.path.join(code_env.local_workplace, "docs")
+        os.makedirs(doc_dir, exist_ok=True)
+        if doc_name.startswith("docs/"):
+            doc_name = doc_name.replace("docs/", "")
+        elif doc_name.startswith("/workspace/docs/"):
+            doc_name = doc_name.replace("/workspace/docs/", "")
+        doc_path = os.path.join(doc_dir, doc_name)
+        assert os.path.exists(doc_path), f"The document `{doc_name}` does not exist in the directory `/workplace/docs`"
+        # the doc_path is a directory
+        if os.path.isdir(doc_path):
+            file_list = []
+            for file in os.listdir(doc_path):
+                if file.endswith(('.pdf', '.docx', '.txt')):
+                    file_list.append(os.path.join(doc_path, file))
+        # the doc_path is a zip file
+        elif os.path.isfile(doc_path) and (doc_path.endswith('.zip') or doc_path.endswith('.tar') or doc_path.endswith('.tar.gz')):
+            file_name = os.path.splitext(doc_name)[0]
+            extract_dir = os.path.join(doc_dir, file_name)
+            os.makedirs(extract_dir, exist_ok=True)
+            
+            with zipfile.ZipFile(doc_path, 'r') as zip_ref:
+                zip_ref.extractall(extract_dir)
+            
+            # 将提取的文件路径添加到file_list中
+            file_list = []
+            for root, _, files in os.walk(extract_dir):
+                for file in files:
+                    if file.endswith(('.pdf', '.docx', '.txt')):
+                        file_list.append(os.path.join(root, file))
+        # the doc_path is a single file
+        elif os.path.isfile(doc_path):
+            file_list.append(doc_path)
+        else:
+            raise ValueError(f"The document `{doc_name}` is not a valid file or directory")
+        
+        mdconvert = MarkdownConverter()
+        
+        ret_val = prefix_res
+        batch_size = 200
+        for file in file_list:
+            queries = []
+            doc_content = mdconvert.convert_local(file).text_content
+            content_chunks = chunking_by_token_size(doc_content, max_token_size=4096)
+            idx_list = ["chunk_" + str(chunk['chunk_order_index']) for chunk in content_chunks]
+            for chunk in content_chunks:
+                query = {
+                    'query': chunk['content'],
+                    'response': f"The {chunk['chunk_order_index']} chunk of the content of the file {file} is: \n{chunk['content']}"
+                }
+                queries.append(query)
+
+            num_batches = math.ceil(len(queries) / batch_size)
+        
+            for i in range(num_batches):
+                start_idx = i * batch_size
+                end_idx = min((i + 1) * batch_size, len(queries))
+                batch_queries = queries[start_idx:end_idx]
+                batch_idx = idx_list[start_idx:end_idx]
+                
+                # Add the current batch of queries
+                memo.add_query(batch_queries, collection=saved_vector_db_name, idx=batch_idx)
+            ret_val += f"The {file} has been added to the vector database `{saved_vector_db_name}`.\n"
+        context_variables["memo"] = memo
+        return Result(
+            value=ret_val,
+            context_variables=context_variables
+        )
+    except Exception as e:
+        ret_val = f"[ERROR] Failed to save the raw documents to the vector database: {e}"
+        return ret_val
+
+
 
 
 def meta_workflow(model: str, context_variables: dict, debug: bool = True):
     print('\033[s\033[?25l', end='')  # Save cursor position and hide cursor
     logger = LoggerManager.get_logger()
-    workflow_generator = get_workflow_generator_agent("openai/qwen-plus")
-    workflow_former = get_workflow_former_agent("openai/qwen-plus")
-    workflow_creator_agent = get_workflow_creator_agent("openai/qwen-plus")
-    # workflow_generator = get_workflow_generator_agent("hosted_vllm/Qwen/QwQ-32B-AWQ")
+    # workflow_generator = get_workflow_generator_agent("openai/qwen-plus")
+    # workflow_former = get_workflow_former_agent("openai/qwen-plus")
+    # workflow_creator_agent = get_workflow_creator_agent("openai/qwen-plus")
+    workflow_generator = get_workflow_generator_agent("hosted_vllm/Qwen/QwQ-32B-AWQ")
     # workflow_former = get_workflow_former_agent("hosted_vllm/Qwen/QwQ-32B-AWQ")
     # workflow_creator_agent = get_workflow_creator_agent("hosted_vllm/Qwen/QwQ-32B-AWQ")
-    # workflow_former = get_workflow_former_agent("hosted_vllm/Qwen/Qwen-32B-Instruct-AWQ")
-    # workflow_creator_agent = get_workflow_creator_agent("hosted_vllm/Qwen/Qwen-32B-Instruct-AWQ")
+    workflow_former = get_workflow_former_agent("hosted_vllm/Qwen/Qwen-32B-Instruct-AWQ")
+    workflow_creator_agent = get_workflow_creator_agent("hosted_vllm/Qwen/Qwen-32B-Instruct-AWQ")
+
+    memo = save_raw_docs_to_vector_db(context_variables, "user_db", "base_info", overwrite=True)
 
     agent = workflow_generator
     agents = {workflow_generator.name.replace(' ', '_'): workflow_generator, workflow_former.name.replace(' ', '_'): workflow_former, workflow_creator_agent.name.replace(' ', '_'): workflow_creator_agent}
